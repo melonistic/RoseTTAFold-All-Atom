c22b38e267c8d9f0e37d63eccb2b2b95410a1509
Santiago Alvarez Vargas <santiago.v.alvarez@gmail.com>
2024-06-03 16:49:06 -0400

    more log entries; removed bash

--- make_msa.sh
+++ make_msa.sh
@@ -131,13 +131,16 @@ fi
     
 echo "Running PSIPRED"
 mkdir -p $out_dir/log
-bash $PIPE_DIR/input_prep/make_ss.sh $out_dir/t000_.msa0.a3m $out_dir/t000_.ss2 > $out_dir/log/make_ss.stdout 2> $out_dir/log/make_ss.stderr
+echo "out_dir/log: $out_dir/log"
+$PIPE_DIR/input_prep/make_ss.sh $out_dir/t000_.msa0.a3m $out_dir/t000_.ss2 > $out_dir/log/make_ss.stdout 2> $out_dir/log/make_ss.stderr
 
 if [ ! -s $out_dir/t000_.hhr ]
 then
     echo "Running hhsearch"
     HH="hhsearch -b 50 -B 500 -z 50 -Z 500 -mact 0.05 -cpu $CPU -maxmem $MEM -aliw 100000 -e 100 -p 5.0 -d $DB_TEMPL"
+    echo "$HH"
     
+    echo "CAT: $out_dir/t000_.ss2 $out_dir/t000_.msa0.a3m > $out_dir/t000_.msa0.ss2.a3m"
     cat $out_dir/t000_.ss2 $out_dir/t000_.msa0.a3m > $out_dir/t000_.msa0.ss2.a3m
     $HH -i $out_dir/t000_.msa0.ss2.a3m -o $out_dir/t000_.hhr -atab $out_dir/t000_.atab -v 0
 fi
